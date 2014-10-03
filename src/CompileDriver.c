5e85963ffb969133ec9abfb4d0f0dcdc88648296
Alvin Lam <alvin.lam@knights.ucf.edu>
2014-10-03 17:29:42 -0400

    Adding cmd into the file

--- src/CompileDriver.c
+++ src/CompileDriver.c
@@ -1,1 +1,18 @@
+/*
+* Alvin Lam
+* Anthony Dinh
+* COP 3402 : System Software
+* October 3rd 2014
+* Assignment 3 : Tiny PL/0 Parser and Code Generator
+*/
 
+#include <CompilerDriver.h>
+
+int main(int argc, char *argv[]){
+
+char cmd;
+scanf("%c", &cmd);
+
+
+return 0;
+}
