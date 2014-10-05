259bb1f8a1e79de4f3d12a93dcbdbac60278fde1
Anthony Dinh <adinh182@knights.ucf.edu>
2014-10-05 09:41:08 -0400

    Added some argument input validation

--- src/CompileDriver.c
+++ src/CompileDriver.c
@@ -12,6 +12,13 @@ int main(int argc, char *argv[]){
 
     FILE *ifp, *ofp1, *ofp2, *ofp3, *ofp4;
 
+    int i;
+
+    //These variables determine whether to print the Lexemes, Assembly code, or the Stack Trace to the screen
+    int printLex = 0;
+    int printPars = 0;
+    int printStack = 0; 
+
 
     //Checks to see if the file exists
     if (!(access( "input.txt", F_OK ) != -1)){
@@ -25,7 +32,62 @@ int main(int argc, char *argv[]){
     ofp3 = fopen("lexemetable.txt", "w");
     ofp4 = fopen("lexemelist.txt", "w");
 
-    if (argc )
+    //Checks to see if the number of arguments is correct
+    if (argc > 4){
+
+        printf("Invalid number of arguments: %d", argc);
+        fprintf(ofp1, "Invalid number of arguments: %d", argc);
+
+        fclose(ifp);
+        fclose(ofp1);
+        fclose(ofp2);
+        fclose(ofp3);
+        fclose(ofp4);
+
+        exit(0);
+    }
+
+    //Checks to see what arguments are passed
+    for (i = 0; i < argc; i++){
+
+        if (argv[i] == 'l'){
+
+            printLex = 1;
+            return;
+        }
+
+        if (argv[i] == 'a'){
+
+            printPars = 1;
+            return;
+        }
+
+        if (argv[i] == 'v'){
+
+            printStack = 1;
+            return;
+        }
+
+        else {
+
+            printf("Invalid argument: %c", argv[i]);
+            fprintf(ofp1, "Invalid argument: %c", argv[i]);
+
+            fclose(ifp);
+            fclose(ofp1);
+            fclose(ofp2);
+            fclose(ofp3);
+            fclose(ofp4);
+
+            exit(0);
+        }
+
+    }
+
+    
+
+
+
 
 
 
